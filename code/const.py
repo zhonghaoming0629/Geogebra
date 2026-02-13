9aa8db162b1d2dcbe3a9849bcb8b0699191e831d
zhong <zhonghaoming2024@163.com>
2026-02-13 10:49:13 +0800

    add some way to create dot and line

---
+++ code/const.py
@@ -0,0 +1,23 @@
+import sys,os,pygame
+
+try:
+    BASIC_PATH  = sys._MEIPASS
+except AttributeError:
+    BASIC_PATH  = os.path.abspath(".")
+
+SELECT_BG_POS = pygame.Vector2()
+SELECT_BG_COLOR= pygame.Color('lightgray')
+
+WEIGH = 800
+HEIGHT = 800
+
+DOT_RADIUS = 10
+
+LINE_WIDTH = 4
+
+UI_TEXT = ['choose','Dot', 'Line']
+UI_NUM = len(UI_TEXT)
+
+SELECTED_COLOR = (0, 255, 255)
+SELECTED_WIDTH = 3
+SELECT_BG_HEIGHT = [0, 600, HEIGHT]
